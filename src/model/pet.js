950775032778f5366c4ab07fd8e451d3c34ac23b
beck <145265944+beckkimball@users.noreply.github.com>
2025-10-04 13:09:52 -0700

    #1 : first draft added pet class structure

---
+++ src/model/pet.js
@@ -0,0 +1,29 @@
+class Pet {
+    constructor(name, species, age, sex, weight) {
+        this.name = name;
+        this.species = species;
+        this.age = age;
+        this.sex = sex;
+        this.weight = weight;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getSpecies() {
+        return this.species;
+    }
+
+    getAge() {
+        return this.age;
+    }
+
+    getSex() {
+        return this.sex;
+    }
+
+    getWeight() {
+        return this.weight;
+    }
+}
